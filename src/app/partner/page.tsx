c7e47f4b1d4bcfe84225536cff685953540b5d17
Philipp <philippderosie@gmail.com>
2025-01-18 11:28:42 +0100

    föreningsloggor

--- src/app/partner/page.tsx
+++ src/app/partner/page.tsx
@@ -1,5 +1,12 @@
 import styles from "./page.module.css";
 import pagesBG from "../assets/images/pagesBG/pagesBG.png";
+import Image from "next/image";
+
+import aspudden from '../assets/images/partner/Aspudden-png.png'
+import borstahusen from '../assets/images/partner/Borstahusen-png.png'
+import djurgårdensHockey from '../assets/images/partner/Djurgårdens-Hockey-png.png'
+import djurgårdensIfUngdom from '../assets/images/partner/DjurgårdensIF-Ungdom-png.png'
+import dösjebro from '../assets/images/partner/Dösjebro-png.png'
 
 import {oswald } from "../layout";
 
@@ -22,12 +29,21 @@ export default function Partner() {
                 Föreningar
               </h2>
               <div className={styles.partnerForeningarGrid}>
-                <div className={styles.foreningBox}></div>
-                <div className={styles.foreningBox}></div>
-                <div className={styles.foreningBox}></div>
-                <div className={styles.foreningBox}></div>
-                <div className={styles.foreningBox}></div>
-                <div className={styles.foreningBox}></div>
+                <div className={styles.foreningBox}>
+                  <Image src={aspudden} alt="Aspudden" />
+                </div>
+                <div className={styles.foreningBox}>
+                  <Image src={borstahusen} alt="Borstahusen" />
+                </div>
+                <div className={styles.foreningBox}>
+                  <Image src={djurgårdensHockey} alt="Djurgårdens Hockey" />
+                </div>
+                <div className={styles.foreningBox}>
+                  <Image src={djurgårdensIfUngdom} alt="Djurgårdens IF Ungdom" />
+                </div>
+                <div className={styles.foreningBox}>
+                  <Image src={dösjebro} alt="Dösjebro" />
+                </div>
                 <div className={styles.foreningBox}></div>
                 <div className={styles.foreningBox}></div>
                 <div className={styles.foreningBox}></div>
